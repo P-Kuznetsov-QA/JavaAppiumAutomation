9933ba6ac77dcc88b9c2b6545696bd361ed02ff0
petrkuznecov <brbrr1000@gmail.com>
2025-09-22 09:33:33 +0300

    Test Result 2

--- src/test/java/MainClassTest.java
+++ src/test/java/MainClassTest.java
@@ -9,5 +9,13 @@ public class MainClassTest extends MainClass
         int actualNumber = this.getLocalNumber();
         Assert.assertEquals("Число " + actualNumber + ", не равно " + expectedNumber , actualNumber, expectedNumber);
     }
+
+    @Test
+    public void testGetClassNumber() {
+        int expectedNumber = 45;
+        int actualNumber = this.getClassNumber();
+        Assert.assertTrue("Число " + actualNumber + ", меньше " + expectedNumber , actualNumber > expectedNumber);
     }
+
+}
 
